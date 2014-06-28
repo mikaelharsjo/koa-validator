9eb3e9bb3452153eca97b1f42fb456037e1fdc22
Mikael Härsjö <mikaelharsjo@gmail.com>
2014-06-28 21:41:37 +0200

    Refactor, move to lib and extract validator

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var router = require('koa-router');
 var request = require('supertest');
 var expect = require('expect.js');
-var koaValidator = require('..');
+var koaValidator = require('../lib');
 var app;
 
 describe('koa-validator()', function(){
@@ -64,6 +64,35 @@ describe('koa-validator()', function(){
 
 			request(app.listen())
 			 	.get('/test?param1=1&param2=test&param3=3')
+			 	.end(done);
+		});
+
+		it('handles multiple errors', function(done){
+			app.use(function *(next){
+				this.checkQuery('param1', 'Valid getparam').isInt();
+				this.checkQuery('param2', 'Not a numeric value').isInt();
+				var errors = this.validationErrors();
+				expect(errors.length).to.equal(2);
+				yield next;
+			});
+
+			request(app.listen())
+			 	.get('/test?param1=abc&param2=test')
+			 	.end(done);
+		});
+
+		it('supports all validators', function(done){
+			app.use(function *(next){
+				this.checkQuery('param1', 'Empty').notEmpty();
+				this.checkQuery('param2', 'Not alpha').isAlpha();
+				this.checkQuery('param3', 'Not null').isNull();
+				var errors = this.validationErrors();
+				expect(errors.length).to.eql(3);
+				yield next;
+			});
+
+			request(app.listen())
+			 	.get('/test?param1=&param2=123&param3=something')
 			 	.end(done);
 		});
 	});
